ff4592e2102e61dc8bf39189972eccc6cff81f0e
Jose R. Borja <chakaitos@gmail.com>
2014-03-06 13:33:13 -0600

    Add failing initialize test

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -1,13 +1,31 @@
 require './pizza'
 
 describe Pizza do
+
+	# describe '.initialize' do
+	#   it 'sets the size of the pizza' do
+	#     pizza = Pizza.new('XL')
+
+	#     expect(pizza.size).to eq('XL')
+	#   end
+	# end
+
   it "exists" do
     expect(Pizza).to be_a(Class)
   end
 end
 
 describe Topping do
+
   it "exists" do
     expect(Topping).to be_a(Class)
   end
+
+	describe '.initialize' do
+	  it 'sets the name of the topping' do
+	    topping = Topping.new('olives')
+
+	    expect(topping.name).to eq('olives')
+	  end
+	end
 end
